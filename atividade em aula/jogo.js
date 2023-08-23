7a41b6d6ca178caf8b929a58de372c3a8be655b6
Marina Laurenio <laureniomarina@gmail.com>
2023-08-23 15:56:57 +0100

    refatoração ativ sala finalizado

--- atividade em aula/jogo.js
+++ atividade em aula/jogo.js
@@ -14,75 +14,10 @@ function iniciaJogo() {
 
   document.getElementById("cronometro").innerHTML = tempoSegundos;
   const quantidadeBaloes = 80;
-
   criaBaloes(quantidadeBaloes);
-
   document.getElementById("baloesInteiros").innerHTML = quantidadeBaloes;
   document.getElementById("baloesEstourados").innerHTML = 0;
-
   contagemTempo(tempoSegundos + 1);
-}
-
-function contagemTempo(segundos) {
-  segundos--;
-
-if(nivel_jogo == 3){//3 dificil -> 30 segs
-    tempo_segundo = 30
-}
-
-document.getElementById('cronometro').innerHTML = tempo_segundo;//inserindo segundos no span
-var qtde_baloes = 80;
-//quantidade de baloes
-
-
-cria_baloes(qtde_baloes);
-
-// imprimir qtde de baloes inteiros
-
-document.getElementById('baloesInteiros').innerHTML = qtde_baloes;
-document.getElementById('baloesEstourados').innerHTML = 0;
-
-contagem_tempo(tempo_segundo + 1)
-}
-
-
-function contagem_tempo(segundos){
-  segundos = segundos - 1;
-
-  if (segundos == -1) {
-    clearTimeout(timerId); // para a execucao da funcao settimeout
-    game_over();
-    return false;
-  }
-  document.getElementById("cronometro").innerHTML = segundos;
-  timerId = setTimeout("contagemTempo(" + segundos + ")", 1000);
-}
-
-function gameOver() {
-  pararJogo()
-  removeEventosBaloes();
-  alert("AAAAAAAAh Acabou o tempo, você não conseguiu!");
-}
-
-function remove_eventos_baloes() {
-    var i = 1; //contado para recuperar balões por id
-
-    //percorre o lementos de acordo com o id e só irá sair do laço quando não houver correspondência com elemento
-    while(document.getElementById('b'+i)) {
-        //retira o evento onclick do elemnto
-        document.getElementById('b'+i).onclick = '';
-        i++; //faz a iteração da variávei i
-    }
-}
-
-
-function situacao_jogo(baloesInteiros){
-  if (baloesInteiros == 0) {
-    alert('UUUUHFA ,aha você conseguiu!');
-    parar_jogo();
-
-
-  }
 }
 
 function criaBaloes(quantidadeBaloes) {
@@ -90,7 +25,7 @@ function criaBaloes(quantidadeBaloes) {
   for (let i = 1; i <= quantidadeBaloes; i++) {
     let balao = document.createElement("img");
     balao.setAttribute("src", "imagens/balao_azul_pequeno.png");
-    balao.classList.add("balaoPequeno");
+    balao.setAttribute("id", `b${i}`);
     balao.addEventListener("click", estourar);
     cenario.appendChild(balao);
   }
@@ -119,11 +54,44 @@ function pontuacao(acao) {
 
 function resultadoJogo(baloesInteiros) {
   if (baloesInteiros === 0) {
-    alert("UUUUFA, você conseguiu!");
-    pararJogo();
+    youWin();
+  }
+}
+
+function contagemTempo(segundos) {
+  segundos--;
+  if (segundos === -1) {
+    clearTimeout(timerId);
+    gameOver();
+    return false;
+  }
+  document.getElementById("cronometro").innerHTML = segundos;
+  timerId = setTimeout(() => contagemTempo(segundos), 1000);
+}
+
+function youWin() {
+  pararJogo();
+  removeEventosBaloes();
+  alert("Você ganhou! Conseguiu estourar todos os balões a tempo!");
+}
+
+function gameOver() {
+  pararJogo();
+  removeEventosBaloes();
+  alert("Que pena! Você não conseguiu estourar todos os balões a tempo!");
+}
+
+function removeEventosBaloes() {
+  var i = 1;
+  while (document.getElementById(`b${i}`)) {
+    const balao = document.getElementById(`b${i}`);
+    balao.removeEventListener("click", estourar);
+    i++;
   }
 }
 
 function pararJogo() {
   clearTimeout(timerId);
 }
+
+window.onload = iniciaJogo;
