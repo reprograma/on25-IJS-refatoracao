0c4ebe9c57d73af66d4dd1e29fb467277d135731
DoriansFer <doohfernandes@yahoo.com>
2023-08-23 22:55:37 -0300

    convertString

--- para-casa/exercicios_DoriaFernandes-Refatorando_Projeto1/Account.js
+++ para-casa/exercicios_DoriaFernandes-Refatorando_Projeto1/Account.js
@@ -35,19 +35,21 @@ class BankAccount {
 
     creditAmount(amount) {
         this.#balance += amount;
-        console.log(`Depósito realizado com sucesso, no valor de R$${amount},00. O novo saldo da conta é: R$ ${this.#balance},00`);
+        console.log(`Depósito realizado com sucesso, no valor de ${this.convertString(amount)}. O novo saldo da conta é: ${this.convertString(this.balance)}`);
         return 'creditado'
+    }
+
+    convertString(string) {
+        const converted = string.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        return converted;
     }
 
     cashWithdrawal(amount) {
         if (amount <= this.balance) {
             this.balance -= amount;
-            console.log(`Sacou o valor de R$${amount},00 `)
-            console.log(`O saldo atual é de R$${this.balance},00. `)
+            console.log(`Sacou o valor de ${this.convertString(amount)}.`)
+            console.log(`O saldo atual é de ${this.convertString(this.balance)}.`)
             return 'ok';
-        } else {
-            console.log(`Você não possui saldo o suficiente`)
-            return 'error saldo'
         }
     }
 
@@ -92,14 +94,14 @@ class BankAccount {
             if (anotherAccount) {
                 this.#balance -= amount;
                 anotherAccount.#balance += amount;
-                console.log(`Pix realizado com sucesso no valor de R$${amount},00, para ${anotherAccount.client.name}. Seu saldo atual é de R$${this.#balance},00`);
+                console.log(`Pix realizado com sucesso no valor de ${this.convertString(amount)}, para ${anotherAccount.client.name}. Seu saldo atual é de ${this.convertString(this.balance)}`);
                 return 'ok'
             } else {
                 console.log(`Chave PIX inválida`);
                 return 'error'
             }
         } else {
-            console.log(`Você não possui saldo o suficiente, seu saldo atual é R$${this.#balance},00`);
+            console.log(`Você não possui saldo o suficiente, seu saldo atual é ${this.convertString(this.balance)}`);
             return 'error'
         }
     }
@@ -109,17 +111,17 @@ class BankAccount {
         const anotherAccount = BankAccount.all.find(acc => acc.accountNumber === account);
         const anotherCpf = BankAccount.all.find(acc => acc.client.cpf === cpf);
         if (amount > transferLimit) {
-            console.log(`Você não possui limite diário disponível pra essa transferência. Seu limite atual é de R$${transferLimit},00`)
+            console.log(`Você não possui limite diário disponível pra essa transferência. Seu limite atual é de ${this.convertString(transferLimit)}`)
             return 'error limite';
         } if (amount > this.#balance) {
-            console.log(`Saldo insuficiente. Seu saldo atual é de R$${this.#balance},00.`);
+            console.log(`Saldo insuficiente. Seu saldo atual é de ${this.convertString(this.balance)}.`);
             return 'error saldo';
         } if (anotherAccount) {
             if (anotherCpf) {
                 this.client.transferLimit -= amount;
                 this.#balance -= amount;
                 anotherAccount.#balance += amount;
-                console.log(`Transferência realizada com sucesso para ${anotherAccount.client.name}, no valor de R$${amount},00. Seu saldo atual é de R$${this.#balance},00.`);
+                console.log(`Transferência realizada com sucesso para ${anotherAccount.client.name}, no valor de ${this.convertString(amount)}. Seu saldo atual é de ${this.convertString(this.balance)}.`);
                 return 'ok';
             } else {
                 console.log("CPF não encontrado");
