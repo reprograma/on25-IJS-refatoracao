24a0aa3cd61116bcdf833a60288aa3fcb1c20be9
guacirita <guacirita@gmail.com>
2023-08-25 15:46:47 -0300

    corigindo a classe premium

--- para-casa/projeto/PremiumAccount/PremiumAccount.js
+++ para-casa/projeto/PremiumAccount/PremiumAccount.js
@@ -18,42 +18,6 @@ class PremiumAccount extends Account {
             throw new Error("Dados inválidos para cadastro");
         }
     }
-
-    transfer(value, accountNumber, agency) {
-        const validAccount = Account.all.find(account => {
-            let accNumber = account.getAccountNumber();
-            let accAgency = account.getAgency();
-            return accNumber === accountNumber && accAgency === agency;
-        });
-        if (!validAccount) {
-            throw new Error("Conta não encontrada");
-        } else if (value < 0) {
-            throw new Error("Valor inválido de transferência");
-        } else if (this.balance - value > 0) {
-            validAccount.setBalance(value);
-            this.balance -= value;
-            return "Transferência feita com sucesso";
-        } else {
-            throw new Error("Você não possui saldo suficiente");
-        }
-    }
-
-    pix(value, pixKey, keyType) {
-        const validAccount = Account.all.find(account => {
-            return account.pixKeys[keyType] === pixKey;
-        });
-        if (!validAccount) {
-            throw new Error("Chave pix não encontrada")
-        } else if (value < 0) {
-            throw new Error("Valor inválido de pix");
-        } else if (this.balance - value > 0) {
-            this.balance -= value;
-            validAccount.setBalance(value);
-            return "Pix feito com sucesso";
-        } else {
-            throw new Error("Você não possui saldo suficiente");
-        }
-    }
 }
 
 export default PremiumAccount;
