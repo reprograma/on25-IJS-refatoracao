baeb7d611f3a8f0cdd690495a3b8a6751fa827b6
Deborah Gomes Souza <deborah270691@gmail.com>
2023-09-01 12:19:43 -0300

    refatoração Gold, Premium e Standard Accounts

--- para-casa/projeto/GoldAccount/GoldAccount.js
+++ para-casa/projeto/GoldAccount/GoldAccount.js
@@ -1,80 +1,71 @@
 import Account from "../Account/Account";
 
 class GoldAccount extends Account {
-  transactionLimit;
-  
+  static TRANSACTION_LIMIT = 5000;
+
   constructor() {
     super();
-    this.transactionLimit = 5000;
   }
 
   createAccount(accountNumber, agency, balance, income) {
-    if (income < 5000 || income > 17999.99) {
-      throw new Error("Renda incompatível com o tipo de conta")
+    if (!this.isValidIncome(income) || !this.isValidAccountData(accountNumber, agency, balance)) {
+      throw new Error("Renda incompatível com o tipo de conta ou dados inválidos para cadastro");
     }
-    if (accountNumber.length === 5 && agency.length === 4 && balance > 0) {
-      this.accountNumber=accountNumber;
-      this.agency=agency;
-      this.balance = balance;
-      this.income = income;
-      return "Conta criada com sucesso";
-    } else {
-      throw new Error("Dados inválidos para cadastro");
-    }
+
+    this.initializeAccount(accountNumber, agency, balance, income);
+    return "Conta criada com sucesso";
   }
 
   transfer(value, accountNumber, agency) {
-    const validAccount = Account.all.find(account => {
-      let accNumber = account.getAccountNumber();
-      let accAgency = account.getAgency();
-      return accNumber === accountNumber && accAgency === agency; 
-    })
+    const validAccount = this.findValidAccount(accountNumber, agency);
 
-    if (!validAccount) {
-      throw new Error ("Conta não encontrada")
+    if (value > GoldAccount.TRANSACTION_LIMIT || value < 0 || !this.hasSufficientBalance(value)) {
+      throw new Error("Transferência inválida ou saldo insuficiente");
     }
 
-    if (value > this.transactionLimit) {
-      throw new Error ("O seu limite de transação é de 1000 reais")
-    }
-
-    if (value < 0) {
-      throw new Error("Valor inválido de transferência");
-    }
-
-    if (this.balance - value > 0) {
-      validAccount.setBalance(value);
-      this.balance -= value;
-      return "Transferência feita com sucesso";
-    } else {
-      throw new Error("Você não possui saldo suficiente");
-    }
+    validAccount.deposit(value);
+    this.withdraw(value);
+    return "Transferência feita com sucesso";
   }
 
   pix(value, pixKey, keyType) {
-    const validAccount = Account.all.find(account => {
-      return account.pixKeys[keyType] === pixKey;
-    })
-  
-    if (!validAccount) {
-      throw new Error ("Chave pix não encontrada")
+    const validAccount = this.findValidAccountByPixKey(pixKey, keyType);
+
+    if (value > GoldAccount.TRANSACTION_LIMIT || value < 0 || !this.hasSufficientBalance(value)) {
+      throw new Error("Pix inválido ou saldo insuficiente");
     }
 
-    if (value > this.transactionLimit) {
-      throw new Error ("O seu limite de transação é de 1000 reais")
-    }
+    this.withdraw(value);
+    validAccount.deposit(value);
+    return "Pix feito com sucesso";
+  }
 
-    if (value < 0) {
-      throw new Error("Valor inválido de pix");
-    }
+  isValidIncome(income) {
+    return income >= 5000 && income <= 17999.99;
+  }
 
-    if (this.balance - value > 0) {
-      this.balance -= value;
-      validAccount.setBalance(value);
-      return "Pix feito com sucesso";
-    } else {
-      throw new Error("Você não possui saldo suficiente");
-    }
+  isValidAccountData(accountNumber, agency, balance) {
+    return accountNumber.length === 5 && agency.length === 4 && balance > 0;
+  }
+
+  initializeAccount(accountNumber, agency, balance, income) {
+    Object.assign(this, { accountNumber, agency, balance, income });
+  }
+
+  findValidAccount(accountNumber, agency) {
+    const validAccount = Account.all.find((account) => account.hasAccountNumberAndAgency(accountNumber, agency));
+    if (!validAccount) throw new Error("Conta não encontrada");
+    return validAccount;
+  }
+
+  findValidAccountByPixKey(pixKey, keyType) {
+    const validAccount = Account.all.find((account) => account.hasPixKey(pixKey, keyType));
+    if (!validAccount) throw new Error("Chave pix não encontrada");
+    return validAccount;
+  }
+
+  hasSufficientBalance(value) {
+    return this.balance - value >= 0;
   }
 }
 
